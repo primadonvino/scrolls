c0326fed802b01616f39b04518dde1cc31c43cf3
Toni Todaro <tonitodaro.mm@gmail.com>
2026-03-17 09:13:04 -0600

    Initial Commit

---
+++ scrolls/scrollsApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrollsApp.swift
+//  scrolls
+//
+//  Created by Toni Todaro on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct scrollsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
